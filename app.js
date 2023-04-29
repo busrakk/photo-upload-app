42da7eac7c1647e7c391e21f0ec4dd0f4e402646
busrakk <busrakartal160@gmail.com>
2023-04-29 03:21:27 +0300

    upload static files

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// MIDDLEWARE
+// express uygulamasında statik dosyaları kullanmak için 'express static'
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send("hello")
-})
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
 const port = 3000;
 
