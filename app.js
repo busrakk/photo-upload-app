44424a22fedcf15e57d3e4e167f1ebf2dd8e3751
busrakk <busrakartal160@gmail.com>
2023-05-02 12:39:34 +0300

    update photo details

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override'); // Tarayıcılar PUT requesti desteklemedikleri için PUT requesti tarayıcının anlayacağı POST request şeklinde simüle etme
 const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
@@ -28,6 +29,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); // url'deki datayı okuma
 app.use(express.json()); // url'deki datayı json çevirme
 app.use(fileUpload()); // express-fileupload
+app.use(methodOverride('_method'));
 
 // ROUTE
 app.get('/', async (req, res) => {
@@ -82,6 +84,24 @@ app.post('/photos', async (req, res) => {
     });
   });
   res.redirect('/');
+});
+
+// edit sayfasına yönlendirme 
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+// bilgi güncelleme
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`)
 });
 
 const port = 3000;
